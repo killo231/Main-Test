ee62f8b8f92b4a2cb14c38e817a2ce9b31b7f311
TNTKanone1 <oke.koenker@gmail.com>
2025-01-27 10:56:13 +0100

    Ich habe eine Methode zum Addieren hinzugefügt.

--- .idea/src/java.java
+++ .idea/src/java.java
@@ -1,5 +1,12 @@
 public class java {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("1 + 2 = " + add(1, 2));
+    }
+
+    private static int add(int a, int b)
+    {
+        int returnValue = a + b;
+        return returnValue;
     }
 }
