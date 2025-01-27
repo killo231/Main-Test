8f9f19fa7080e6c6aa2cba1cc37ee3449641f58d
enes <enestunjj@gmail.com>
2025-01-27 10:53:16 +0100

    SIgma Boy

---
+++ .idea/src/Hallo.java
@@ -0,0 +1,15 @@
+public class Hallo {
+
+    public static void main(String[] args) {
+        UwU();
+    }
+
+    static void UwU()
+    {
+        while (true)
+        {
+            System.out.println("UwU");
+        }
+    }
+
+}
