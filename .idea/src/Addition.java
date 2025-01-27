7eafea9427f36dafe9ec4ad9acbf24f1c4cdfa52
TNTKanone1 <oke.koenker@gmail.com>
2025-01-27 11:15:03 +0100

    Ich habe eine Klasse und eine Methode zum Addieren hinzugefügt.

---
+++ .idea/src/Addition.java
@@ -0,0 +1,12 @@
+/*
+Klasse zum addieren von 2 Zahlen
+@auth Oke Könker
+27.01.2025
+ */
+public class Addition
+{
+    public int addition(int zahl1, int zahl2)
+    {
+        return zahl1 + zahl2;
+    }
+}
